7c991f976714afab1ef17454a8ddb2282bb57b65
Michael Greenberg <michael.greenberg@pomona.edu>
2020-07-14 16:04:42 -0700

    basic syntax

---
+++ src/lib.rs
@@ -0,0 +1,144 @@
+use std::cmp::PartialEq;
+
+/// gamma
+#[derive(Clone, Debug, PartialEq)]
+pub enum BaseType {
+    Bool,
+    Int,
+}
+
+/// alpha
+pub type TypeVariable = usize;
+
+/// T
+#[derive(Clone, Debug, PartialEq)]
+pub enum StaticType {
+    Base(BaseType),
+    Var(TypeVariable),
+    Fun(Box<StaticType>, Box<StaticType>),
+}
+
+/// G
+#[derive(Clone, Debug, PartialEq)]
+pub enum GradualType {
+    Base(BaseType),
+    Var(TypeVariable),
+    Fun(Box<GradualType>, Box<GradualType>),
+    Dyn(),
+}
+
+/// c
+#[derive(Clone, Debug, PartialEq)]
+pub enum Constant {
+    Bool(bool),
+    Int(usize),
+}
+
+/// x
+pub type Variable = String;
+
+/// e (ITGL)
+#[derive(Clone, Debug)]
+pub enum Expr {
+    Const(Constant),
+    Var(Variable),
+    Lam(Variable, Box<Expr>),
+    LamDyn(Variable, Box<Expr>),
+    App(Box<Expr>, Box<Expr>),
+    If(Box<Expr>, Box<Expr>, Box<Expr>),
+    // TODO operations on constants
+}
+
+impl Expr {
+    pub fn lam(v: Variable, e: Expr) -> Expr {
+        Expr::Lam(v, Box::new(e))
+    }
+
+    pub fn lam_dyn(v: Variable, e: Expr) -> Expr {
+        Expr::LamDyn(v, Box::new(e))
+    }
+    pub fn app(e1: Expr, e2: Expr) -> Expr {
+        Expr::App(Box::new(e1), Box::new(e2))
+    }
+
+    pub fn if_(e1: Expr, e2: Expr, e3: Expr) -> Expr {
+        Expr::If(Box::new(e1), Box::new(e2), Box::new(e3))
+    }
+}
+
+impl GradualType {
+    pub fn fun(g1: GradualType, g2: GradualType) -> GradualType {
+        GradualType::Fun(Box::new(g1), Box::new(g2))
+    }
+
+    pub fn consistent(&self, other: &GradualType) -> bool {
+        match (self, other) {
+            (GradualType::Dyn(), _) | (_, GradualType::Dyn()) => true,
+            (GradualType::Fun(g11, g12), GradualType::Fun(g21, g22)) => {
+                g11.consistent(g21) && g12.consistent(g22)
+            }
+            (g1, g2) => g1 == g2,
+        }
+    }
+
+    pub fn dom(&self) -> Option<GradualType> {
+        match self {
+            GradualType::Dyn() => Some(GradualType::Dyn()),
+            GradualType::Fun(g1, _) => Some(*g1.clone()),
+            _ => None,
+        }
+    }
+
+    pub fn cod(&self) -> Option<GradualType> {
+        match self {
+            GradualType::Dyn() => Some(GradualType::Dyn()),
+            GradualType::Fun(_, g2) => Some(*g2.clone()),
+            _ => None,
+        }
+    }
+
+    /// should only be called on consistent types
+    pub fn meet(&self, other: &GradualType) -> GradualType {
+        match (self, other) {
+            (GradualType::Dyn(), g) | (g, GradualType::Dyn()) => g.clone(),
+            (GradualType::Fun(g11, g12), GradualType::Fun(g21, g22)) => {
+                GradualType::fun(g11.meet(g21), g12.meet(g22))
+            }
+            (g1, g2) => {
+                assert_eq!(g1, g2, "meet is only defined on consistent types");
+                g1.clone()
+            },
+        }
+    }
+
+    pub fn try_meet(&self, other: &GradualType) -> Option<GradualType> {
+        match (self, other) {
+            (GradualType::Dyn(), g) | (g, GradualType::Dyn()) => Some(g.clone()),
+            (GradualType::Fun(g11, g12), GradualType::Fun(g21, g22)) => {
+                let g1 = g11.try_meet(g21)?;
+                let g2 = g12.try_meet(g22)?;
+                Some(GradualType::fun(g1, g2))
+            }
+            (g1, g2) => {
+                if g1 == g2 {
+                    Some(g1.clone())
+                } else {
+                    None
+                }
+            },
+        }
+    }
+}
+
+impl From<&StaticType> for GradualType {
+    fn from(t: &StaticType) -> Self {
+        match t {
+            StaticType::Base(b) => GradualType::Base(b.clone()),
+            StaticType::Var(a) => GradualType::Var(a.clone()),
+            StaticType::Fun(t1, t2) => GradualType::fun(
+                GradualType::from(t1.as_ref()),
+                GradualType::from(t2.as_ref()),
+            ),
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate mgt;
+
+use mgt::*;
+
 fn main() {
-    println!("Hello, world!");
+    let x = Expr::Var(String::from("x"));
+    let little_omega = Expr::lam(String::from("x"), Expr::app(x.clone(), x));
+    let big_omega = Expr::app(little_omega.clone(), little_omega.clone());
+    println!("{:?}", big_omega);
 }
